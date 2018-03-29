323ee62e668a542aee9030914c69ad2d7b76dc5a
Chaitanya Bendalam <chaitanya.0507@gmail.com>
2018-03-29 20:15:59 +0530

    Added the ignore files into repositiory, changed the base file path

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Importing `index` Controller from `controller/index.js` file
+// Importing `index` handler from `handler/index.js` file
 const handler = require('../handler/index');
 // Include Joi package to validate request params and payload.
 const Joi = require('joi');
@@ -8,9 +8,9 @@ const Joi = require('joi');
 module.exports = function() {
 	return [
     {
-			method: 'GET', 																	// Methods Type
-			path: '/guessit/{id}', 														// Url
-			handler: handler.printGuessItOutput 					// Action for fetching categories Information
+			method: 'GET', 															// Methods Type
+			path: '/guessit/{id}', 											// Url
+			handler: handler.printGuessItOutput 				// Action for fetching result
 		}
 	];
 }();
